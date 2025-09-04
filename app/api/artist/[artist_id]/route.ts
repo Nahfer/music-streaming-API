f4c28a86a1a6542e66376b58ff1ddfdcf6f401a0
Nah fer <nahfereal@gmail.com>
2025-09-04 14:39:48 +0300

    flushed error on /api/artist/[artist_id] route

--- app/api/artist/[artist_id]/route.ts
+++ app/api/artist/[artist_id]/route.ts
@@ -49,7 +49,7 @@ export async function GET(
 
     console.error("Error fetching artist data:", error);
     return NextResponse.json(
-      { error: "Internal server error" },
+      { error: (error instanceof Error) ? error.message : "Internal server error" },
       { status: 500 }
     );
   }
